e77d413b9ec5c6b2cc3d0ea0ed0899a6fb55941c
junbo.zhao <wy989899@163.com>
2023-07-16 01:11:57 +0800

    var

---
+++ src/go_code/chapter02/homework03/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*\t\t")
+	fmt.Println("*\t*\t\t\t\tI love Golang \t\t\t\t\t*\t*")
+	fmt.Println("\t*\t\t\t\t\t\t\t\t\t\t\t\t*\t")
+	fmt.Println("\t\t*\t\t\t\t\t\t\t\t\t\t*\t\t")
+	fmt.Println("\t\t\t*\t\t\t\t\t\t\t\t*\t\t\t")
+	fmt.Println("\t\t\t\t*\t\t\t\t\t\t*\t\t\t\t")
+	fmt.Println("\t\t\t\t\t*\t\t\t\t*\t\t\t\t\t")
+	fmt.Println("\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t")
+	fmt.Println("\t\t\t\t\t\t\t*\t\t\t\t\t\t\t")
+
+}
